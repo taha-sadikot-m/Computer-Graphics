2f87da692b1537e853610ce540f0c9bd438e5f1e
Taha Sadikot <taha.sadikot.m@gmail.com>
2025-08-01 15:14:06 +0530

    Updated circle theta code

---
+++ DDA_CIRCLE/theta_Circle.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Center coordinates
+xc = 340
+yc = 280
+
+# Input radius
+r = int(input("Enter radius of circle: "))
+xc = int(input("Enter x-coordinate of center: "))
+yc = int(input("Enter y-coordinate of center: "))
+
+# Generate theta values from 0 to 2*pi
+theta = np.arange(0, 2 * np.pi, 0.1)
+
+# Calculate x and y coordinates for the circle
+x = xc + r * np.cos(theta)
+y = yc + r * np.sin(theta)
+
+
+# Plot the circle points
+plt.plot(x, y, color='b')
+plt.scatter(xc, yc, color='r', label='Center')
+
+plt.title("Circle using Parametric (Theta) Method")
+plt.xlabel("X-axis")
+plt.ylabel("Y-axis")
+plt.axis('equal')
+plt.grid(True)
+plt.legend()
+plt.show()
